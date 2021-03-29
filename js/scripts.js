95de84abb3fce52bf3ca9d6d90d2802e415e531a
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-29 14:12:19 -0700

    FIX: populate list items into array groceryList
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 // business logic
-let groceryList = [];
-let groceryListUpper = [];
 
 // user interface logic
 $(document).ready(function() {
   $("#grocery-items").submit(function(event) {
-    $(groceryList).push("#item1");
-    $(groceryList).push("#item2");
-    $(groceryList).push("#item3");
-    $(groceryList).push("#item4");
-    $(groceryList).push("#item5");
+    event.preventDefault();
+    
+    let groceryList = [];
+    let groceryListUpper = [];
+
+    groceryList.push($("#item1").val());
+    groceryList.push($("#item2").val());
+    groceryList.push($("#item3").val());
+    groceryList.push($("#item4").val());
+    groceryList.push($("#item5").val());
+
     console.log(groceryList);
 
     groceryList.sort();
+
     console.log(groceryList);
 
-    groceryList.forEach(function(event) {
-      event.toUpperCase();
-      console.log(groceryList);
-    });
+
+
+    // groceryList.forEach(function(event) {
+    //   event.toUpperCase();
+    //   console.log(groceryList);
+    // });
   });
 });
       
